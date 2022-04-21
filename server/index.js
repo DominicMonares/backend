aa70e3706833e80603a5f8c8a1b6202a765cae2c
Joseph Nahm <josephnahm1@gmail.com>
2022-04-20 18:33:15 -0700

    fixed mongo uri

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(passport.initialize());
 const MongoStore = require ('connect-mongo')(session);
 
 
-const dbString = 'mongodb://localhost:27017/sessions';
+const dbString = `mongodb://${process.env.uri}/sessions`;
 const dbOptions = {
   useNewUrlParser: true,
   useUnifiedTopology:true
