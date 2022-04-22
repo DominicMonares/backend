6fd746870cdb0cf18325adabc743a334ff29f731
troyqyang <troy.q.yang@gmail.com>
2022-04-22 16:42:24 -0700

    Resetting MongoStore

--- server/index.js
+++ server/index.js
@@ -21,8 +21,8 @@ app.use(passport.initialize());
 const MongoStore = require ('connect-mongo')(session);
 
 
-const dbString = 'mongodb://localhost:27017/sessions';
-// const dbString = `${process.env.uri}/sessions`;
+
+const dbString = `${process.env.uri}/sessions`;
 const dbOptions = {
   useNewUrlParser: true,
   useUnifiedTopology:true
