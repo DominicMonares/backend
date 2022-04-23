a210929168507061c77666b0f6a4d78454c45d15
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-04-22 17:35:10 -0700

    replace callbackURL

--- config/passport.js
+++ config/passport.js
@@ -41,7 +41,7 @@ passport.use(new LocalStrategy(function verify(username, password, cb) {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/oauth2/redirect/google',
+  callbackURL: 'http://54.215.206.56.nip.io/oauth2/redirect/google',
   scope: [
     'https://www.googleapis.com/auth/userinfo.profile',
     'https://www.googleapis.com/auth/userinfo.email'
